66743a033d605a25a521acb6ca4d8650941e525a
Roger <aileshang0226@163.com>
2021-04-13 14:41:53 +0800

    save 处理光标颜色问题

--- lib/pages/home/add_ dynamic.dart
+++ lib/pages/home/add_ dynamic.dart
@@ -6,6 +6,8 @@ class AddDyNamic extends StatefulWidget {
 }
 
 class _AddDyNamicState extends State<AddDyNamic> {
+  TextEditingController contentController = TextEditingController();
+
   final Color activeColor = Color.fromRGBO(51, 51, 51, 1.0);
 
   @override
@@ -28,14 +30,26 @@ class _AddDyNamicState extends State<AddDyNamic> {
               ),
             ),
             onPressed: () {
-              print('发表动态');
+              String content = contentController.text;
+              print(content);
             },
           )
         ],
       ),
       body: Container(
         margin: EdgeInsets.all(5.0),
-        child: Text('内容'),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: '输入内容',
+            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent, width: 0)),
+          ),
+          autofocus: false,
+          controller: contentController,
+          cursorColor: Colors.black, //光标颜色
+          maxLength: 500,
+          minLines: 10,
+          maxLines: 10,
+        ),
       ),
     );
   }
